b49a3b95e2f307c7c5193db8d13b970aca4c4872
magomedov_sultan <sultanvortik@gmail.com>
2026-05-06 01:17:46 +0300

    bug fix

--- js/timer.js
+++ js/timer.js
@@ -41,7 +41,6 @@ function secondsCalculation() {
         timerValues[2] -= 1
         currentTime.innerHTML = `${timerValues[0]}:${timerValues[1]}:${timerValues[2]}`
         timerBlock.classList.add('timer-start') 
-        console.log(timerValues)
     } else if (timerValues[2] === 0 && timerValues[1] > 0) {
         minutesCalculation()
     } else if (timerValues[0] > 0 && timerValues[1] === 0 && timerValues[2] === 0) {
@@ -62,7 +61,6 @@ function secondsCalculation() {
 }
 
 let secondsInt;
-console.log(secondsInt)
 
 function playTimer() {
     if (secondsInt === undefined) {
@@ -70,9 +68,7 @@ function playTimer() {
         secondsInt = setInterval(() => {
             secondsCalculation()
         }, 1000)
-        console.log(secondsInt)
     } else {
-        console.log(secondsInt)
         return
     }
 }
